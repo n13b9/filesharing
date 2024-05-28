ec761cfa62b9d4e5558af0a0e9783f0ba25dc08a
n13b9 <neeraj.barla92@gmail.com>
2024-05-28 12:33:39 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -25,19 +25,20 @@ const server = createServer(app);
 //     }
 //  });
 
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL,
-//     methods: ['GET', 'POST'],
-//     credentials: true // Allow credentials
-// }));
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.header("Access-Control-Allow-Origin", FRONTEND_URL);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
-
 app.use(bodyParser.json());
 app.use(
   cookieParser({
