27f94909ea974f7ff680744841f7765a6f66839f
n13b9 <neeraj.barla92@gmail.com>
2024-05-28 13:10:37 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -25,16 +25,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  console.log("Request received:", req.method, req.url);
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://filesharing-two.vercel.app/"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 app.use(bodyParser.json());
 app.use(
   cookieParser({
