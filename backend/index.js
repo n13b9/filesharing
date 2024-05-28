9dc802436a4e079df704cd4ae45887f59aaaca69
n13b9 <neeraj.barla92@gmail.com>
2024-05-28 12:15:50 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,55 +1,61 @@
-const express = require('express');
-const http = require('http');
-const {Server} = require('socket.io')
-const sokcetIO = require('socket.io');
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const { createServer } = require('node:http');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const sokcetIO = require("socket.io");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { createServer } = require("node:http");
 
-const authRoutes = require('./routes/authRoutes')
-const fileShareRoutes = require('./routes/fileShareRoutes')
+const authRoutes = require("./routes/authRoutes");
+const fileShareRoutes = require("./routes/fileShareRoutes");
 
-require('./db');
-require('./models/userModel')
-require('./models/verificationModel');
+require("./db");
+require("./models/userModel");
+require("./models/verificationModel");
 
-
-const PORT = process.env.PORT_ID; 
+const PORT = process.env.PORT_ID;
 
 const app = express();
 const server = createServer(app);
 
-// const io = new Server(server, { 
+// const io = new Server(server, {
 //     cors:{
 //         origin:process.env.FRONTEND_URL
 //     }
 //  });
- 
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST'],
-    credentials: true // Allow credentials
-}));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL,
+//     methods: ['GET', 'POST'],
+//     credentials: true // Allow credentials
+// }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(bodyParser.json());
-app.use(cookieParser({
+app.use(
+  cookieParser({
     httpOnly: true,
     secure: true,
-    sameSite: 'none',
+    sameSite: "none",
     maxAge: 1000 * 60 * 60 * 24 * 7,
     signed: true,
-}));
-app.use('/public', express.static('public'));
+  })
+);
+app.use("/public", express.static("public"));
 
+app.use("/auth", authRoutes);
+app.use("/file", fileShareRoutes);
 
-app.use('/auth',authRoutes);
-app.use('/file',fileShareRoutes);
-
-app.use('/',(req,res)=>{
-    res.send('API is working ....')
-})
+app.use("/", (req, res) => {
+  res.send("API is working ....");
+});
 
 // io.on("connection", (socket) => {
 //     console.log('new connection',socket.id);
@@ -64,21 +70,17 @@ app.use('/',(req,res)=>{
 //     socket.on('uploaded', (data) => {
 //                 let sender = data.from;
 //                 let receiver = data.to;
-        
+
 //                 console.log('uploaded', data);
-        
-        
+
 //                 socket.to(receiver).emit('notify', {
 //                     from: sender,
 //                     message: 'New file shared'
 //                 })
-//     })    
+//     })
 
 //   });
 
-
-  
-server.listen(PORT,()=>{
-    console.log('server is running at' + PORT )
-})
-
+server.listen(PORT, () => {
+  console.log("server is running at" + PORT);
+});
